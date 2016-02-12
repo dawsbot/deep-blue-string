b1275fe44bb5cb861600129bce47d81cc00619ea
dawsonbotsford <dawsonbotsford@gmail.com>
2016-02-12 01:26:51 -0700

    Better documentation Handle null Test and checklist for complex object types

--- test.js
+++ test.js
@@ -22,20 +22,19 @@ test('#throws', t => {
 
 test('#regexFunctions', t => {
   t.is(dbs('yoo', String.prototype.replace, [/o/g, '']), 'y');
-  // TODO: test String.prototype.search
+  t.is(dbs('yoo', String.prototype.search, [/o/g]), 1);
 });
 
 test('#stringFunctions', t => {
   t.is(dbs('UNIcorns', String.prototype.toLowerCase), 'unicorns');
   t.is(dbs('unicorns', String.prototype.replace, ['uni', '']), 'corns');
-  t.is(dbs('uNICorns', String.prototype.toLowerCase), 'unicorns');
   t.is(dbs('uni', String.prototype.repeat, [3]), 'uniuniuni');
+  t.is(dbs('yoo', String.prototype.search, /o/g), 1);
 });
 
 test('#basicArrayFunctions', t => {
   t.same(dbs(['UNIcorns'], String.prototype.toLowerCase), ['unicorns']);
   t.same(dbs(['unicorns'], String.prototype.replace, ['uni', '']), ['corns']);
-  t.same(dbs(['uNICorns'], String.prototype.toLowerCase), ['unicorns']);
 });
 
 test('#complexArrayFunctions', t => {
@@ -45,7 +44,6 @@ test('#complexArrayFunctions', t => {
 
 test('#basicObjectFunctions', t => {
   t.same(dbs({unicorns: 'unicycle'}, String.prototype.replace, ['uni', '']), {corns: 'cycle'});
-  t.same(dbs({unicorns: 'unicycle'}, String.prototype.toUpperCase), {UNICORNS: 'UNICYCLE'});
   t.same(dbs({unicorns: 'unicycle', unit: 'university'}, String.prototype.replace, ['uni', '']), {corns: 'cycle', t: 'versity'});
 });
 
@@ -53,3 +51,27 @@ test('#complexMixedDatatypeFunctions', t => {
   t.same(dbs([{unicorns: 'unicycle'}], String.prototype.replace, ['uni', '']), [{corns: 'cycle'}]);
   t.same(dbs(['hackcu', {unicorns: 'unicycle'}, 'test'], String.prototype.toUpperCase), ['HACKCU', {UNICORNS: 'UNICYCLE'}, 'TEST']);
 });
+
+test('#dontModifyTargetNatives', t => {
+  // Don't modify native types
+  t.same(dbs(123, String.prototype.replace, ['123', '']), 123);
+  t.same(dbs(true, String.prototype.replace, ['t', '']), true);
+  t.same(dbs(undefined, String.prototype.replace, ['u', '']), undefined);
+  t.same(dbs(null, String.prototype.replace, ['u', '']), null);
+
+  const sym = Symbol('test');
+  t.same(dbs(sym, String.prototype.replace, ['t', '']), sym);
+
+  const emptyFunc = () => {
+    return true;
+  };
+  t.same(dbs(emptyFunc, String.prototype.replace, ['t', '']), emptyFunc);
+});
+
+/*
+test('#dontModifyTargetObjects', t => {
+  // Don't modify native types
+  const date = new Date();
+  t.same(dbs(date, String.prototype.toUpperCase), date);
+});
+*/
